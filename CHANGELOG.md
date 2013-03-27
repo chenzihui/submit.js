7f6a9591fc7acb7bf2c5d063ea356c1c73da8fc8
Chen Zihui <hello@chenzihui.com>
2013-03-27 21:17:09 +0800

    Add support for older versions of IE

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.2.0
+--------------------
+- Add support for IE7, IE8
+
 0.1.0
 --------------------
 - Support for major Firefox & Chrome browsers
--- submit.js
+++ submit.js
@@ -9,38 +9,50 @@
 (function() {
   'use strict';
 
-  // Add a submit handler to the document object, we're using
-  // event delegation here.
-  document.addEventListener( 'submit', function( e ) {
+  /*
+   * Handles the submit event
+   *
+   * @method handleSubmitEvent
+   */
+
+  var handleSubmitEvent = function( e ) {
     e.preventDefault();
 
-    var els = e.target.children,
-        len = els.length,
-        i, prevVal;
+      var els = e.target.children,
+          len = els.length,
+          i, prevVal;
 
-    // Iterate through the form's children to find the
-    // submit button
-    for ( i = 0; i < len; i++ ) {
-      if ( els[i].hasAttribute( 'data-submit' ) ) {
-        prevVal = els[i].value;
+      // Iterate through the form's children to find the
+      // submit button
+      for ( i = 0; i < len; i++ ) {
+        if ( els[i].hasAttribute( 'data-submit' ) ) {
+          prevVal = els[i].value;
 
-        // Set the button's value to the one specified in the data- attribute
-        els[i].value = els[i].getAttribute( 'data-submit' );
+          // Set the button's value to the one specified in the data- attribute
+          els[i].value = els[i].getAttribute( 'data-submit' );
 
-        // Set the previous value to the data- attribute so that you can
-        // reuse it in your AJAX callbacks.
-        //
-        // E.g.
-        //
-        // $.post( url, data, function( res ) {
-        //   $( '#mySubmitButton' ).val( $( '#mySubmitButton' ).attr( 'data-submit' ) );
-        // });
+          // Set the previous value to the data- attribute so that you can
+          // reuse it in your AJAX callbacks.
+          //
+          // E.g.
+          //
+          // $.post( url, data, function( res ) {
+          //   $( '#mySubmitButton' ).val( $( '#mySubmitButton' ).attr( 'data-submit' ) );
+          // });
 
-        els[i].setAttribute( 'data-submit' );
+          els[i].setAttribute( 'data-submit' );
 
-        // Assuming only one submit button in the form
-        break;
+          // Assuming only one submit button in the form
+          break;
+        }
       }
-    }
-  });
+  };
+
+  if ( document.addEventListener ) {
+    // Add a submit handler to the document object, we're using
+    // event delegation here.
+    document.addEventListener( 'submit', handleSubmitEvent );
+  } else {
+    document.attachEvent( 'onsubmit', handleSubmitEvent );
+  }
 })();
